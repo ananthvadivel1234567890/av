629c9982fb0d6c10aa3d5ac089bf580c4dcd6c84
ananthvadivel1234567890 <35652942+ananthvadivel1234567890@users.noreply.github.com>
2018-03-01 16:24:04 +0530

    Create n array.c

---
+++ n array.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+   int a,b,c,n,i;
+   printf("enter the values of n\n");   
+   scanf("%d", &n);
+   for(i=0;i<=n;i++)
+   scanf("%d",&a[i]);
+   for(i=0;i<=n;i++)
+   {
+       for(j=i;j<n;j++)
+   }
+   {
+           if(a[i]>a[j])
+               a[i]=a[j];
+           }
+   printf("%d",a[i]);
+}
